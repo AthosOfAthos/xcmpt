2cd9c62862b2aa986e0aa0ac6db40fddff9922cd
AthosOfAthos <athosofathos@gmail.com>
2023-04-05 23:46:00 -0500

    Prototype component access

--- src/ecs.rs
+++ src/ecs.rs
@@ -1,6 +1,8 @@
 
+use core::any::TypeId;
+
+use crate::storage::Slot;
 use crate::{Component, ComponentRegistry, component::ComponentID, storage::ComponentArray};
-use alloc::rc::Rc;
 use alloc::vec::Vec;
 use hashbrown::HashMap;
 use runtime_id::RuntimeID;
@@ -78,29 +80,70 @@ impl ECS {
 	}
 
 	pub fn has_component<C: Component>(&self, entity: &EntityID) -> bool {
-		unimplemented!()
+		if !self.is_valid(entity) { return false; }
+		if self.components.contains_key(&TypeId::of::<C>()) {
+			let component_array = self.components.get(&TypeId::of::<C>()).unwrap();
+			let array = unsafe { component_array.get_slice::<C>() };
+			match array[entity.index] {
+			    Slot::Empty => return false,
+			    Slot::Some(_) => return true,
+			}
+		}
+		false
 	}
 
 	pub fn add_component<C: Component>(&mut self, entity: &EntityID, component: C) {
-		unimplemented!()
+		if !self.is_valid(entity) { return; }
+		let component_id = TypeId::of::<C>();
+		if self.components.contains_key(&component_id) {
+			let component_array = self.components.get_mut(&component_id).unwrap();
+			let array = unsafe { component_array.get_slice_mut::<C>() };
+			array[entity.index] = Slot::Some(component);
+		}
 	}
 
-	pub fn remove_component<C: Component>(&mut self, entity: &EntityID) -> Option<C> {
-		unimplemented!()
+	pub fn remove_component<C: Component>(&mut self, entity: &EntityID) {
+		if !self.is_valid(entity) { return; }
+		let component_id = TypeId::of::<C>();
+		if self.components.contains_key(&component_id) {
+			let component_array = self.components.get_mut(&component_id).unwrap();
+			let array = unsafe { component_array.get_slice_mut::<C>() };
+			array[entity.index] = Slot::Empty;
+		}
 	}
 
 	pub fn get_component<C: Component>(&self, entity: &EntityID) -> Option<&C> {
-		unimplemented!()
+		if !self.is_valid(entity) { return None; }
+		let component_id = TypeId::of::<C>();
+		if self.components.contains_key(&component_id) {
+			let component_array = self.components.get(&component_id).unwrap();
+			let array = unsafe { component_array.get_slice::<C>() };
+			match &array[entity.index] {
+			    Slot::Empty => return None,
+			    Slot::Some(component) => return Some(component),
+			}
+		}
+		None
 	}
 
 	pub fn get_component_mut<C: Component>(&mut self, entity: &EntityID) -> Option<&mut C> {
-		unimplemented!()
+		if !self.is_valid(entity) { return None; }
+		let component_id = TypeId::of::<C>();
+		if self.components.contains_key(&component_id) {
+			let component_array = self.components.get_mut(&component_id).unwrap();
+			let array = unsafe { component_array.get_slice_mut::<C>() };
+			match &mut array[entity.index] {
+			    Slot::Empty => return None,
+			    Slot::Some(component) => return Some(component),
+			}
+		}
+		None
 	}
 }
 
 #[cfg(test)]
 mod test {
-	use crate::{ComponentRegistry, ECS};
+	use crate::{ComponentRegistry, ECS, Component};
 
 	#[test]
 	fn basic() {
@@ -111,5 +154,29 @@ mod test {
 		assert!(ecs.is_valid(&entity));
 		ecs.destroy_entity(entity);
 		assert!(!ecs.is_valid(&entity));
+	}
+
+	#[derive(PartialEq, Eq)]
+	struct TestComponent(usize);
+	impl Component for TestComponent {}
+
+	#[test]
+	fn component_basic() {
+		let mut registry = ComponentRegistry::new();
+		registry.register::<TestComponent>();
+		let mut ecs = ECS::new(&registry);
+
+		let entity = ecs.create_entity();
+		assert!(!ecs.has_component::<TestComponent>(&entity));
+		let test_component = TestComponent(128);
+		ecs.add_component(&entity, test_component);
+		assert!(ecs.has_component::<TestComponent>(&entity));
+
+		ecs.get_component_mut::<TestComponent>(&entity).unwrap().0 = 72;
+		let component = ecs.get_component::<TestComponent>(&entity).unwrap();
+		assert_eq!(component.0, 72);
+		
+		ecs.remove_component::<TestComponent>(&entity);
+		assert!(!ecs.has_component::<TestComponent>(&entity));
 	}
 }
